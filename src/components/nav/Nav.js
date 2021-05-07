ec18ac9c07a445b8f31f5d2bc4aab4b7979679a1
guptankit6252 <ankitgupta6252@gmail.com>
2021-05-07 10:51:06 +0530

    added by ankit

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import './nav.css'
+import { Button } from '../button/Button'
+
+function Nav() {
+
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+    
+    window.addEventListener('resize', showButton);
+
+    return (
+        <>
+            <nav className = 'navbar'>
+                <div className = 'navbar-container'>
+                    <Link to='/' className = 'navbar-logo' onClick = {closeMobileMenu}>
+                        BLOGS
+                        <i class = 'fab fa-typo3' />
+                    </Link>
+                    <div className = 'menu-icon' onClick = {handleClick}>
+                        <i className = {click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className = 'nav-item'>
+                            <Link to='/' className = 'nav-links' onClick = {closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className = 'nav-item'>
+                            <Link to='/writeBlog' className = 'nav-links' onClick = {closeMobileMenu}>
+                                Write 
+                            </Link>
+                        </li>
+                        <li className = 'nav-item'>
+                            <Link to='/readBlog' className = 'nav-links' onClick = {closeMobileMenu}>
+                                Read
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/SignUp' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Nav
---
+++ src/components/readblog/readBlog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function readBlog() {
+    return (
+        <div>
+            <h1>CHOOSE TO READ</h1>
+        </div>
+    )
+}
+
+export default readBlog
